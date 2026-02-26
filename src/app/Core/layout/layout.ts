98b2792dcd092f0006b9bc84ff2357ac1d8ff763
Nahum <nahummejiad@gmail.com>
2026-02-26 00:25:55 -0600

    Added: Notification Service to centralize toast on the apps

--- src/app/Core/layout/layout.ts
+++ src/app/Core/layout/layout.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Navbar } from "./navbar/navbar";
 import { Footer } from "./footer/footer";
+import { PrimeNGModule } from '../../Shared/PrimeNG/prime-ng.imports';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer, PrimeNGModule, RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    MessageService,
     providePrimeNG({
       theme: {
         preset: Aura,
