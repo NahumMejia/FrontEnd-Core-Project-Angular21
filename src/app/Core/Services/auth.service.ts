4165049b763b104d45c774bda0cda9597d381509
Nahum <nahummejiad@gmail.com>
2026-02-27 21:28:00 -0600

    Added: Mock Auth Service for implement later

---
+++ src/app/Core/Services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+
+  //TODO: Make the service right when u have the spring boot backend
+
+  public login(){
+    
+  }
+  public logOut(){
+    
+  }
+  public getToken(){
+    
+  }
+  public isLogged(){
+    
+  }
+}
