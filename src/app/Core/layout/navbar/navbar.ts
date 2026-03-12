f7c7b6f3c31ad82fc1f946f26bb94a8f748a83ed
Nahum <nahummejiad@gmail.com>
2026-03-12 17:34:30 -0600

    Added: Logout to the navbar

--- src/app/Core/layout/navbar/navbar.ts
+++ src/app/Core/layout/navbar/navbar.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { ToolbarModule } from 'primeng/toolbar';
 import { MenubarModule } from 'primeng/menubar';
+import { MenuModule } from 'primeng/menu';
+import { AvatarModule } from 'primeng/avatar';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [ToolbarModule, MenubarModule],
+  imports: [ToolbarModule, MenubarModule, MenuModule, AvatarModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar implements OnInit {
+  private readonly authService = inject(AuthService);
+
   public items: MenuItem[] | undefined;
+  public userMenuItems: MenuItem[] | undefined;
 
   ngOnInit(): void {
     this.items = [
@@ -23,6 +29,16 @@ export class Navbar implements OnInit {
           { label: 'Components', icon: 'pi pi-bolt' },
           { label: 'UI Kit', icon: 'pi pi-pencil' },
         ],
+      },
+    ];
+
+    this.userMenuItems = [
+      {
+        label: 'Cerrar sesión',
+        icon: 'pi pi-sign-out',
+        command: () => {
+          this.authService.logout();
+        },
       },
     ];
   }
