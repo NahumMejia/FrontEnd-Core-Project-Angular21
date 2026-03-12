20a04ac339302dd369eadc687202b7b6eb066505
Nahum <nahummejiad@gmail.com>
2026-03-11 21:49:55 -0600

    Added: Toast on the app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { PrimeNGModule } from "./shared/primeNG/prime-ng.imports";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrimeNGModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
